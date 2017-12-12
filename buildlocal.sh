e3a3f7d3e773300174166c8cfb7bf91ea55b5702
Tuur Lievens <tuur.lievens@hotmail.com>
2017-12-12 22:11:28 +0100

    Add fonts when building local app

--- buildlocal.sh
+++ buildlocal.sh
@@ -2,9 +2,15 @@
 # Angular build
 rm -rf dist
 ng build -prod --base-href "./"
-sed -i -e 's#<base href=\".\/\">#<script>document.write(`<base href="${document.location}" \/>`)<\/script>#g' dist/index.html
 
+# rewrite base element so app works locally
 # <base href="./">
 # <script>document.write(`<base href="${document.location}" />`)</script>
+sed -i -e 's#<base href=\".\/\">#<script>document.write(`<base href="${document.location}" \/>`)<\/script>#g' dist/index.html
+
+# add fonts
+sed -i -e 's#<\/head>#<link href="localfonts.css" rel="stylesheet"></head>#g' dist/index.html
+cp localassets/* dist
+
 
 echo -e "\n\n Built!\n"
