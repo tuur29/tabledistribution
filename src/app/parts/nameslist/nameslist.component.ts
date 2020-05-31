2023a17920f6f79da31637cda82f1be35e8882c2
Tuur Lievens <tuur.lievens@hotmail.com>
2020-05-31 20:31:51 +0200

    Allow sorting of participants list

--- src/app/parts/nameslist/nameslist.component.ts
+++ src/app/parts/nameslist/nameslist.component.ts
@@ -34,7 +34,7 @@ import 'rxjs/add/operator/map';
           <input matInput placeholder="Search names" aria-label="Search names" [formControl]="namesCtrl">
 
           <button *ngIf="namesCtrl.value" matSuffix mat-icon-button aria-label="Reset" (click)="namesCtrl.reset()">
-           <mat-icon>close</mat-icon>
+            <mat-icon>close</mat-icon>
           </button>
 
         </mat-form-field>
@@ -42,8 +42,15 @@ import 'rxjs/add/operator/map';
 
       <mat-list>
         <mat-list-item *ngFor="let person of filteredList | async">
-          <span [style.color]="person.data.color">{{person.name}} <span *ngIf="person.data.letter">({{person.data.letter}})</span></span>
+          <span [style.color]="person.data.color">
+            {{person.name}}
+            <span *ngIf="person.data.letter">({{person.data.letter}})</span>
+          </span>
         </mat-list-item>
+
+        <button *ngIf="true" mat-icon-button aria-label="Sort" (click)="toggleSort()">
+            <mat-icon>sort</mat-icon>
+          </button>
       </mat-list>
 
     </mat-expansion-panel>
@@ -75,12 +82,23 @@ import 'rxjs/add/operator/map';
       height: 32px;
     }
 
+    mat-list {
+      position: relative;
+    }
+
+    mat-list button {
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
+
   `]
 })
 export class NamesListComponent implements OnInit {
 
   list = [];
   @LocalStorage("hideNamesList") hide = false;
+  @LocalStorage("sortNameAlphabetically") sortNameAlphabetically: boolean = false;
 
   // variables
   namesCtrl: FormControl = new FormControl();
@@ -91,25 +109,42 @@ export class NamesListComponent implements OnInit {
   ngOnInit() {
     this.filteredList = this.namesCtrl.valueChanges
       .startWith(null)
-      .map(query => query ? this.filter(query) : this.list.slice());
+      .map(query => this.sort(query ? this.filter(query) : this.list.slice()));
   }
 
   filter(query: string) {
-    return this.list.filter(person =>
-      this.normalize(person.name).indexOf(this.normalize(query)) > -1 ||
+    const filteredList = this.list.filter(person =>
+      this.normalize(person.name).includes(this.normalize(query)) ||
       this.normalize(person.data.letter).indexOf(this.normalize(query)) > -1
     );
+    return filteredList;
+  }
+
+  sort(input: any[]) {
+    if (this.sortNameAlphabetically) {
+      return input.sort((a, b) => {
+        // if (a.name === b.name) return 0; // @Jan
+        return a.name > b.name ? 1 : -1;
+      });
+    }
+
+    return input;
+  }
+
+  toggleSort() {
+    this.sortNameAlphabetically = !this.sortNameAlphabetically;
+    this.namesCtrl.setValue(this.namesCtrl.value); // updates filteredList
   }
 
   updateTable(groups: any[]) {
     this.list = [];
     for (let i=0; i<groups.length;i++)
       this.list = this.list.concat(groups[i]);
-    
-    this.namesCtrl.setValue('');
+
+    this.namesCtrl.setValue(''); // updates filteredList
   }
 
-  private normalize(s: string) {
+  private normalize(s: string): string {
     return removeDiacritics(s.toLowerCase());
   }
 }
