309256adaf72b9603344b5de4a0c2cd49e3bd1aa
Tuur Lievens <tuur.lievens@hotmail.com>
2020-05-31 20:42:42 +0200

    Remove duplicate explanation closes #5

--- src/app/parts/explanation/explanation.component.ts
+++ src/app/parts/explanation/explanation.component.ts
@@ -13,7 +13,7 @@ import { LocalStorage } from 'ngx-store';
 
       <mat-expansion-panel-header>
         <mat-panel-title>
-          
+
           <ng-container *ngIf="!globals.auth.token;else loggedin">
             <h1>Login</h1>
           </ng-container>
@@ -26,7 +26,7 @@ import { LocalStorage } from 'ngx-store';
 
       <p>This tool was made to easily spread participants across a number of tables during a number of rounds so that everybody only meets eachother the least possible times.</p>
       <ol>
-        <li>Start by selecting the size of the grid. If you have a larger amount of tables than rounds select the amount of tables and ignore the other rounds.<br>If you have more tables than rounds you will have to assign the letters of the extra tables to the rounds yourself.</li>
+        <li>Start by selecting the size of the grid. If you have a larger amount of tables than rounds select the amount of tables and ignore the other rounds.</li>
         <li>Spread the names of all participants across all letters as much as possible.
           <br>Participants with the same letter will always be seated together.
           <br>Participants with letters in the same column will never sit at the same table.</li>
